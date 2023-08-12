7e7ed27a4f92645a2116ff8ae53f4669519a15ec
Reza <rezafadilah337@gmail.com>
2023-08-12 23:47:29 +0700

    final style

--- js/blog.js
+++ js/blog.js
@@ -4,8 +4,9 @@ function addBlog(event) {
   event.preventDefault();
 
   let title = document.getElementById("input-blog-title").value;
-  let startDate = document.getElementById("input-blog-startDate").value
-  let endDate = document.getElementById("input-blog-endDate").value
+  let startDate = document.getElementById("input-blog-startDate").value;
+  let endDate = document.getElementById("input-blog-endDate").value;
+  let input = document.querySelectorAll(".multi-input:checked");
   let content = document.getElementById("input-blog-content").value;
   let image = document.getElementById("input-blog-image").files;
 
@@ -39,12 +40,29 @@ function addBlog(event) {
     duration = years + " Tahun";
   }
 
-  
+  const nodeJsIcon = '<i class="fa-brands fa-node"></i>';
+  const reactIcon = '<i class="fa-brands fa-square-js"></i>';
+  const javaIcon = '<i class="fa-brands fa-react"></i>';
+  const typeIcon = '<i class="fa-brands fa-node-js"></i>';
+
+  let nodeJs = document.getElementById("input-chk-node").checked ? nodeJsIcon : "";
+  let nextJs = document.getElementById("input-chk-next").checked ? reactIcon : "";
+  let reactJs = document.getElementById("input-chk-react").checked ? javaIcon : "";
+  let typeScript = document.getElementById("input-chk-ts").checked ? typeIcon : "";
+
+  let multiInput = document.querySelectorAll(".multi-input:checked")
+  if (multiInput.length === 0) {
+    return alert("Please Select At least One Technologies")
+  }
 
   let blog = {
     title,
     duration,
     content,
+    nodeJs,
+    nextJs,
+    reactJs,
+    typeScript,
     image,
   };
 
@@ -61,19 +79,20 @@ function renderBlog() {
                 <div class="container-card">
                     <div class="card-content">
                         <img src="${dataBlog[index].image}" alt="gambar">
-                        <h1>
-                            <a href="blog-detail.html" target="_blank">
+                        <a href="blog-detail.html" target="_blank">
+                          <h1>
                             ${dataBlog[index].title}
-                            </a>
-                        </h1>
+                          </h1>
+                        </a>
                         <p>durasi : ${dataBlog[index].duration}</p>
                         <div id="container-desc">
                         ${dataBlog[index].content}
                         </div>
                         <div>
-                          <i class="fa-brands fa-google-play"></i>
-                          <i class="fa-brands fa-android"></i>
-                          <i class="fa-brands fa-java"></i>
+                          ${dataBlog[index].nodeJs}
+                          ${dataBlog[index].nextJs}
+                          ${dataBlog[index].reactJs}
+                          ${dataBlog[index].typeScript}
                         </div>
                         <div class="ctn-btn">
                             <div class="btn-left">
@@ -88,3 +107,4 @@ function renderBlog() {
         `
     }
 }
+
